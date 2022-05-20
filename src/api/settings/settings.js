8050df165317f88c3d5733083035068765a5c3d8
Jerson Janke <jerson.janke@saudeid.com.br>
2022-05-20 11:31:29 -0300

    fix: change name autocomplete for autoplay and remove type

--- src/api/settings/settings.js
+++ src/api/settings/settings.js
@@ -3,8 +3,7 @@ const mongoose = restful.mongoose
 
 const settings = new mongoose.Schema({
   email: { type: String, required: true },
-  type: { type: String, required: true },
-  autocomplete: { type: Boolean, required: true, default: false },
+  autoplay: { type: Boolean, required: true, default: false },
   microphone: { type: Boolean, required: true, default: false },
 })
 
