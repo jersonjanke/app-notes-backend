b216992e03df82fb574f5900b04aa629a9f86064
Jerson Janke <jerson.janke@saudeid.com.br>
2022-05-20 10:39:25 -0300

    feat: add email for settings

--- src/api/settings/settings.js
+++ src/api/settings/settings.js
@@ -2,6 +2,7 @@ const restful = require('node-restful')
 const mongoose = restful.mongoose
 
 const settings = new mongoose.Schema({
+  email: { type: String, required: true },
   type: { type: String, required: true },
   autocomplete: { type: Boolean, required: true, default: false },
   microphone: { type: Boolean, required: true, default: false },
