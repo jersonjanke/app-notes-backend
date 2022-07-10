2c18339abd0dccdfd2fb657fbf93841dfc30ebf0
Jerson Janke <jerson.janke@saudeid.com.br>
2022-07-10 14:20:27 -0300

    Auth2 Google validate token

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,8 @@
 const jwt = require('jsonwebtoken')
+const { OAuth2Client } = require('google-auth-library')
+const client = new OAuth2Client(
+  '829625549487-m7dvtef9obacodq027b6j7eegt3p6oon.apps.googleusercontent.com'
+)
 
 module.exports = (req, res, next) => {
   // CORS preflight request
@@ -12,15 +16,27 @@ module.exports = (req, res, next) => {
       return res.status(403).send({ errors: ['No token provided.'] })
     }
 
-    jwt.verify(token, process.env.SECRET, function (err, decoded) {
-      if (err) {
-        return res.status(403).send({
-          errors: ['Failed to authenticate token.'],
-        })
-      } else {
-        // req.decoded = decoded
-        next()
-      }
-    })
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: '829625549487-m7dvtef9obacodq027b6j7eegt3p6oon.apps.googleusercontent.com',
+    });
+
+    const payload = ticket.getPayload();
+    const userid = payload['sub'];
+
+    if (userid) {
+      next()
+    } else {
+      jwt.verify(token, process.env.SECRET, function (err, decoded) {
+        if (err) {
+          return res.status(403).send({
+            errors: ['Failed to authenticate token.'],
+          })
+        } else {
+          // req.decoded = decoded
+          next()
+        }
+      })
+    }
   }
 }
